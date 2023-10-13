c1d497884d7f9a180c4f2f166b1f1beceb691f08
ArtaxJay <okjaceauthor@gmail.com>
2023-10-13 11:08:27 +0100

    Modify 0*

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
-res = add(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, res))
+
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    added_values = add(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, added_values))
