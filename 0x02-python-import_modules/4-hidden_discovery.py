36f9df480c50638244ade980c86810ccf2697d28
ArtaxJay <okjaceauthor@gmail.com>
2023-10-13 11:44:06 +0100

    Modify 4-*

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,8 @@
-import py_compile
+#!/usr/bin/python3
 
-# Compile the .pyc file to .py
-py_compile.compile('hidden_4.pyc')
-
-# Import the module and list its names
-import hidden_4
-
-for nm in dir(hidden_4):
-    if not nm.startswith("__"):
-        print(nm)
+if __name__ == "__main__":
+    import hidden_4
+    nms = dir(hidden_4)
+    for nm in nms:
+        if nm[:2] != "__":
+            print(nm)
