7866751ce26e709ed2b8ca3b60866daaf1f07753
ArtaxJay <okjaceauthor@gmail.com>
2023-10-13 11:36:20 +0100

    Modify 3-*

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-import sys
 
-args = sys.argv[1:]
-rez = 0
-
-for arg in args:
-    rez += int(arg)
-
-print(rez)
+if __name__ == "__main__":
+    import sys
+    ttl = 0
+    for i in range(len(sys.argv) - 1):
+        ttl += int(sys.argv[i + 1])
+    print("{}".format(ttl))
