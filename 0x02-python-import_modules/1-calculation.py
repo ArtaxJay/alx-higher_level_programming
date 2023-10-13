ca7cb20eb3037ddab773a5ccc953137b9172f534
ArtaxJay <okjaceauthor@gmail.com>
2023-10-13 11:21:36 +0100

    Modify 1-*

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
 
-a = 10
-b = 5
-
-add_rez = add(a, b)
-sub_rez = sub(a, b)
-mul_rez = mul(a, b)
-div_rez = div(a, b)
-
-print("{} + {} = {}".format(a, b, add_rez))
-print("{} - {} = {}".format(a, b, sub_rez))
-print("{} * {} = {}".format(a, b, mul_rez))
-print("{} / {} = {}".format(a, b, div_rez))
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
