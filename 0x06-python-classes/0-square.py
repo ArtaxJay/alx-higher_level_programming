fe28ce5ef73fac23d491ebb549464ee45fec0715
ArtaxJay <okjaceauthor@gmail.com>
2023-10-25 02:29:06 +0100

    Modify 0-*

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
-"""Inside this codebase
-    a class named Square is defiened.
-"""
+"""Inside this codebasena class named Square is defiened."""
 
 class Square:
     """A void blueprint code to represent a square."""
