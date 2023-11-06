3161aece9d1701de433858e1aba4b0f5bdbff177
ArtaxJay <okjaceauthor@gmail.com>
2023-11-06 15:44:09 +0100

    Add 2-*

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Lorem ipsum dolor."""
+
+
+def is_same_class(obj, a_class):
+    """Lorem ipsum dolor sit amet consectetur.
+
+    Args:
+        obj (class): lorem ipsum.
+        a_class (instance): instance dolor sit.
+    Returns:
+        True if obj == a_class.
+        Else returns False.
+    """
+
+    if isinstance(a_class, obj):
+        return True
+    else:
+        return False
