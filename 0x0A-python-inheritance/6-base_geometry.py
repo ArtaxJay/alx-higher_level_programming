ea8da8ec72565902542709a1d08e5c9efe16d7c0
ArtaxJay <okjaceauthor@gmail.com>
2023-11-06 16:56:46 +0100

    Add 6-*

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Create empty BaseGeometry class."""
+
+
+class BaseGeometry:
+    """Reps a mathematical geometry."""
+
+    def area(self):
+        """Area is not implemented."""
+        raise Exception("area() is not implemented")
