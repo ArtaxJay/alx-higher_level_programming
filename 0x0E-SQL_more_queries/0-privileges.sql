646547052adcf00093f896eb09536dd996b65f77
ArtaxJay <okjaceauthor@gmail.com>
2023-12-14 09:55:46 +0100

    Add 0-*

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- This SQL script will list all privileges of the
+-- MySQL users user_0d_1 & user_0d_2
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,6 @@
+# 0x0E. SQL - More queries
+
+This is the second project on the SQL learning path.
+
+This dir contains answers attempted by:
+# Ola-Olwajuwon
