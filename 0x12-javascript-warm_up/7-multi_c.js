463ad9dbc0618f101c30b7a34d9771c46dda962a
ArtaxJay <okjaceauthor@gmail.com>
2024-01-08 21:02:23 +0100

    Add 7-*

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+if (process.argv[2] === undefined || isNaN(parseInt(process.argv[2])) === true) {
+	console.log('Missing number of occurrences');
+} else {
+	for (let i = 1; i <= parseInt(process.argv[2]); i++) {
+		console.log('C is fun');
+	}
+}
