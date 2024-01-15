50fae039bc8942d2801e0e626c503d76ff1525a1
ArtaxJay <okjaceauthor@gmail.com>
2024-01-15 11:36:09 +0100

    First commit. Add a README file

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Outputs all the states from hbtn_0e_0_usa DB."""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    DBname = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], DBname=sys.argv[3], port=3306)
+    my_cursor = DBname.cursor()
+    my_cursor.execute("SELECT * FROM states")
+    tbl_records = my_cursor.fetchall()
+    for record in tbl_records:
+        print(record)
+    my_cursor.close()
+    DBname.close()
