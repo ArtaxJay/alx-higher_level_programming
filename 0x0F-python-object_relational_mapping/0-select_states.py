02c92894ea59408fb368c03f647d70489c31cec3
ArtaxJay <okjaceauthor@gmail.com>
2024-01-15 15:38:53 +0100

    Modifies 0-*

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""Outputs all the states from hbtn_0e_0_usa DB."""
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    DBname = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                             passwd=sys.argv[2], DBname=sys.argv[3], port=3306)
-    my_cursor = DBname.cursor()
-    my_cursor.execute("SELECT * FROM states")
-    tbl_records = my_cursor.fetchall()
-    for record in tbl_records:
-        print(record)
-    my_cursor.close()
-    DBname.close()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
