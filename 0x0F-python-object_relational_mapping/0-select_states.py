7d482f7c6a178655afccaa2f9949c80de627ae31
ArtaxJay <okjaceauthor@gmail.com>
2024-01-15 11:40:49 +0100

    Add 0-select*

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,7 @@ import sys
 
 if __name__ == "__main__":
     DBname = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], DBname=sys.argv[3], port=3306)
+                             passwd=sys.argv[2], DBname=sys.argv[3], port=3306)
     my_cursor = DBname.cursor()
     my_cursor.execute("SELECT * FROM states")
     tbl_records = my_cursor.fetchall()
