0bc988d06f80cc3f55a4d4c6055e444676006b81
ArtaxJay <okjaceauthor@gmail.com>
2024-01-15 17:02:21 +0100

    Add 1-*

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Outputs all the states from hbtn_0e_0_usa DB."""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    DBname = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                             passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    my_cursor = DBname.cursor()
+    my_cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                      .format(sys.argv[4]))
+    tbl_records = my_cursor.fetchall()
+    for record in tbl_records:
+        print(record)
+    my_cursor.close()
+    DBname.close()
