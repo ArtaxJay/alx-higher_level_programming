406eaacd090a52ad5691eb190e8b9bbd4add8a50
ArtaxJay <okjaceauthor@gmail.com>
2024-01-29 23:57:45 +0100

    Add 0-*

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,8 @@ using the py module urllib.request.
 if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as http_response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as \
+            http_response:
         http_message = http_response.read()
         print("Body response:")
         print("\t- type: {}".format(type(http_message)))
