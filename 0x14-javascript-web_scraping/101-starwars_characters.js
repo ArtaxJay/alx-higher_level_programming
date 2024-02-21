d21148c19f756c21078d1960c4db3dbaf83ce46f
ArtaxJay <okjaceauthor@gmail.com>
2024-02-21 05:20:47 +0100

    Add 101-*

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,40 @@
+#!/usr/bin/node
+
+const req = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+req(apiUrl, (err, res, msg) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const movie = JSON.parse(msg);
+  const charsUrls = movie.characters;
+  
+  // Counter to keep track of how many characters have been printed
+  let counter = 0;
+
+  // Function to print character name and move to the next character
+  const printChar = (i) => {
+    if (i >= charsUrls.length) {
+      return;
+    }
+    const charUrl = charsUrls[i];
+    req(charUrl, (err, res, msg) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      const chars = JSON.parse(msg);
+      console.log(chars.name);
+      
+      // Print the next character recursively
+      printChar(i + 1);
+    });
+  };
+
+  // Start printing characters from the first character
+  printChar(0);
+})
